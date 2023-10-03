8cdfe93d0638ed084b58473d039c1d21061d7757
Fgoldarlar <fgoldarlar@educacion.navarra.es>
2023-10-03 20:31:06 +0200

    Create Actividad10.py

---
+++ Actividad10.py
@@ -0,0 +1,6 @@
+PMuñeca = 75
+PPayaso = 112
+Payaso = int(input("Numero de payasos a enviar: "))
+Muñeca = int(input("Numero de muñecas a enviar: "))
+PTotal = PMuñeca * Muñeca + PPayaso * Payaso
+print = ("El peso total de la entrega es", PTotal)
